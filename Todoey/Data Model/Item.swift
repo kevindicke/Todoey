88133b4242d67ef6039cef0fe3a908bffd7422fe
Kevin Dicke <k.dicke7@gmail.com>
2019-03-26 13:34:52 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kevin Dicke on 3/26/19.
+//  Copyright © 2019 Kevin Dicke. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
